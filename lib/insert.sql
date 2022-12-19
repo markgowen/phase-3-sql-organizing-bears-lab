649b9cae55f9cd3eb60cbe85e1d2e9e3c0cdcf52
Mark <mark.gowen78@gmail.com>
2022-12-19 14:20:14 -0600

    Lab solution

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, "Mr. Chocolate", 25, "M", "brown", "calm", 1);
+INSERT INTO bears VALUES (2, "Rowdy", 25, "M", "black", "rowdy", 1);
+INSERT INTO bears VALUES (3, "Tabitha", 5, "F", "light brown", "curious", 0);
+INSERT INTO bears VALUES (4, "Sergeant Brown", 35, "M", "brown", "intense", 1);
+INSERT INTO bears VALUES (5, "Melissa", 10, "F", "brown", "weird", 1);
+INSERT INTO bears VALUES (6, "Grinch", 2, "M", "black", "mean", 1);
+INSERT INTO bears VALUES (7, "Wendy", 20, "F", "brown", "calm", 0);
+INSERT INTO bears VALUES (8, null, 25, "M", "brown", "aggressive", 1);
